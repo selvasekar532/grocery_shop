f9021eadf41998cede7b255c289571c2cde21762
Lingesh machine <selvasekar532@gmail.com>
2023-07-08 12:04:47 +0530

    completed login form

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ let shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
 }
+
+    //  for login page
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
