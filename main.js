f85d6b2726e587e413d04318d5825ce97ef9548e
Lingesh machine <selvasekar532@gmail.com>
2023-07-08 08:15:41 +0530

    shopping-cart updated

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+    //  for search bar
+
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+}
+    // for shopping cart
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
 }
